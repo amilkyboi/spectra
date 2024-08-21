d1f421be4b2a5365e4669a3c2107cb5fb5b977cd
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-08-21 13:34:45 -0500

    add table support for multiple bands

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,7 @@
 Testing GUI functionality.
 """
 
+import warnings
 import tkinter as tk
 from tkinter import ttk
 from typing import Callable
@@ -28,11 +29,15 @@ from simulation import Simulation
 # The fixes in the linked thread don't seem to work, which is why I think the issue is internal to
 # pandastable itself. For now, just disable the warning.
 # https://stackoverflow.com/questions/77900971/pandas-futurewarning-downcasting-object-dtype-arrays-on-fillna-ffill-bfill
-pd.set_option('future.no_silent_downcasting', True)
+pd.set_option("future.no_silent_downcasting", True)
+# More warnings for d_type conversions that aren't yet fixed in a release build of pandastable, see:
+# https://github.com/dmnfarrell/pandastable/issues/251
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 DEFAULT_TEMPERATURE: float = 300.0    # [K]
 DEFAULT_PRESSURE:    float = 101325.0 # [Pa]
 
+DEFAULT_BANDS:    str = "0-0"
 DEFAULT_PLOTTYPE: str = "Line"
 DEFAULT_SIMTYPE:  str = "Absorption"
 
@@ -90,6 +95,12 @@ class MolecularSimulationGUI:
         self.frame_input: ttk.Frame = ttk.Frame(self.root)
         self.frame_input.pack(side=tk.BOTTOM, fill=tk.X, padx=5, pady=5)
 
+        self.frame_input_entries: ttk.Frame = ttk.Frame(self.frame_input)
+        self.frame_input_entries.pack(side=tk.LEFT, fill=tk.X, padx=5, pady=5)
+
+        self.frame_input_combos: ttk.Frame = ttk.Frame(self.frame_input)
+        self.frame_input_combos.pack(side=tk.RIGHT, fill=tk.X, padx=5, pady=5)
+
         # Frame for the table and plot
         self.frame_main: ttk.Frame = ttk.Frame(self.root)
         self.frame_main.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=5, pady=5)
@@ -104,34 +115,46 @@ class MolecularSimulationGUI:
 
         # Entries ----------------------------------------------------------------------------------
 
-        ttk.Label(self.frame_input, text="Temperature [K]:").pack(side=tk.LEFT, padx=5, pady=5)
-        self.temperature: tk.DoubleVar = tk.DoubleVar(value=DEFAULT_TEMPERATURE)
-        ttk.Entry(self.frame_input, textvariable=self.temperature).pack(side=tk.LEFT, padx=5, pady=5)
+        self.temperature = tk.DoubleVar(value=DEFAULT_TEMPERATURE)
+        ttk.Label(self.frame_input_entries, text="Temperature [K]:").grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        ttk.Entry(self.frame_input_entries, textvariable=self.temperature).grid(row=0, column=1, padx=5, pady=5)
 
-        ttk.Label(self.frame_input, text="Pressure [Pa]:").pack(side=tk.LEFT, padx=5, pady=5)
-        self.pressure: tk.DoubleVar = tk.DoubleVar(value=DEFAULT_PRESSURE)
-        ttk.Entry(self.frame_input, textvariable=self.pressure).pack(side=tk.LEFT, padx=5, pady=5)
+        self.pressure = tk.DoubleVar(value=DEFAULT_PRESSURE)
+        ttk.Label(self.frame_input_entries, text="Pressure [Pa]:").grid(row=0, column=2, padx=5, pady=5, sticky="w")
+        ttk.Entry(self.frame_input_entries, textvariable=self.pressure).grid(row=0, column=3, padx=5, pady=5)
+
+        self.band_ranges = tk.StringVar(value=DEFAULT_BANDS)
+        ttk.Label(self.frame_input_entries, text="Band Ranges (format: v'-v''):").grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        ttk.Entry(self.frame_input_entries, textvariable=self.band_ranges, width=50).grid(row=1, column=1, columnspan=3, padx=5, pady=5)
 
         # Comboboxes -------------------------------------------------------------------------------
 
-        ttk.Label(self.frame_input, text="Simulation Type:").pack(side=tk.LEFT, padx=5, pady=5)
-        self.simulation: tk.StringVar = tk.StringVar(value=DEFAULT_SIMTYPE)
-        ttk.Combobox(self.frame_input, textvariable=self.simulation, values=("Absorption", "Emission")).pack(side=tk.LEFT, padx=5, pady=5)
+        self.simulation = tk.StringVar(value=DEFAULT_SIMTYPE)
+        ttk.Label(self.frame_input_combos, text="Simulation Type:").grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        ttk.Combobox(self.frame_input_combos, textvariable=self.simulation, values=("Absorption", "Emission")).grid(row=0, column=1, padx=5, pady=5)
 
-        ttk.Label(self.frame_input, text="Plot Type:").pack(side=tk.LEFT, padx=5, pady=5)
-        self.plot_type: tk.StringVar = tk.StringVar(value=DEFAULT_PLOTTYPE)
-        ttk.Combobox(self.frame_input, textvariable=self.plot_type, values=("Line", "Line Info", "Convolution", "Band Info")).pack(side=tk.LEFT, padx=5, pady=5)
+        self.plot_type = tk.StringVar(value=DEFAULT_PLOTTYPE)
+        ttk.Label(self.frame_input_combos, text="Plot Type:").grid(row=0, column=2, padx=5, pady=5, sticky="w")
+        ttk.Combobox(self.frame_input_combos, textvariable=self.plot_type, values=("Line", "Line Info", "Convolution", "Band Info")).grid(row=0, column=3, padx=5, pady=5)
 
         # Button -----------------------------------------------------------------------------------
 
-        ttk.Button(self.frame_input, text="Run Simulation", command=self.run_simulation).pack(side=tk.LEFT, padx=5, pady=5)
+        ttk.Button(self.frame_input, text="Run Simulation", command=self.run_simulation).pack()
+
+        # Notebook ---------------------------------------------------------------------------------
+
+        # Holds multiple tables, one for each specified vibrational band
+        self.notebook = ttk.Notebook(self.frame_table)
+        self.notebook.pack(fill=tk.BOTH, expand=True)
 
         # Table ------------------------------------------------------------------------------------
 
         # Initialize the table with an empty dataframe so that nothing is shown until a simulation
         # is run by the user
-        self.table = Table(self.frame_table, dataframe=pd.DataFrame(), showtoolbar=True, showstatusbar=True, editable=False)
-        self.table.show()
+        tab_frame = ttk.Frame(self.notebook)
+        table = Table(tab_frame, dataframe=pd.DataFrame(), showtoolbar=True, showstatusbar=True, editable=False)
+        table.show()
+        self.notebook.add(tab_frame, text="Band v'-v''")
 
         # Plot -------------------------------------------------------------------------------------
 
@@ -154,7 +177,32 @@ class MolecularSimulationGUI:
             "Band Info": plot.plot_band_info
         }
 
-    def run_simulation(self):
+    def parse_band_ranges(self) -> list[tuple[int, int]]:
+        """
+        Convert comma-separated user input of the form 0-0, 0-2, etc. into valid vibrational bands.
+        """
+
+        band_ranges_str: str = self.band_ranges.get()
+
+        bands: list[tuple[int, int]] = []
+
+        for range_str in band_ranges_str.split(','):
+            range_str: str = range_str.strip()
+
+            if '-' in range_str:
+                try:
+                    lower_band: int
+                    upper_band: int
+                    lower_band, upper_band = map(int, range_str.split('-'))
+                    bands.append((lower_band, upper_band))
+                except ValueError:
+                    print(f"Invalid band range format: {range_str}")
+            else:
+                print(f"Invalid band range format: {range_str}")
+
+        return bands
+
+    def run_simulation(self) -> None:
         """
         Runs a simulation instance.
         """
@@ -164,8 +212,8 @@ class MolecularSimulationGUI:
         pressure:    float = self.pressure.get()
         # Convert to uppercase to use as a key for the SimType enum
         sim_type:    str   = self.simulation.get().upper()
-
-        bands: list[tuple[int, int]] = [(2, 0)]
+        # Upper and lower vibrational bands
+        bands: list[tuple[int, int]] = self.parse_band_ranges()
 
         molecule:   Molecule   = Molecule("o2", 'o', 'o')
         simulation: Simulation = Simulation(molecule, temperature, pressure, np.arange(0, 36),
@@ -189,21 +237,37 @@ class MolecularSimulationGUI:
         self.axs.legend()
         self.plot_canvas.draw()
 
-        # Create a dictionary of data for each line
-        data: list[dict[str, int | str]] = [
-            {
-                "J'": line.rot_qn_up,
-                "J''": line.rot_qn_lo,
-                "branch": line.branch_name,
-                "n'": line.branch_idx_up,
-                "n''": line.branch_idx_lo
-            }
-            for line in simulation.vib_bands[0].lines
-        ]
+        # Clear previous tabs
+        for tab in self.notebook.tabs():
+            self.notebook.forget(tab)
 
-        # Update the table's internal dataframe model with the new data
-        self.table.model.df = pd.DataFrame(data)
-        self.table.redraw()
+        # Get maximum intensity for the entire vibrational band range for normalization
+        max_intensity: float = simulation.all_line_data()[1].max()
+
+        # Each vibrational band has a separate tab associated with it, each tab gets updated
+        # separately
+        for i, band in enumerate(bands):
+            data: list[dict[str, float | int | str]] = [
+                {
+                    "Wavenumber": 0.0,
+                    "Intensity": 0.0,
+                    "J'": line.rot_qn_up,
+                    "J''": line.rot_qn_lo,
+                    "Branch": line.branch_name,
+                    "N'": line.branch_idx_up,
+                    "N''": line.branch_idx_lo
+                }
+                for line in simulation.vib_bands[i].lines
+            ]
+
+            df = pd.DataFrame(data)
+            df["Wavenumber"] = simulation.vib_bands[i].wavenumbers_line()
+            df["Intensity"]  = simulation.vib_bands[i].intensities_line() / max_intensity
+
+            tab_frame: ttk.Frame = ttk.Frame(self.notebook)
+            table: Table = Table(tab_frame, dataframe=df, showtoolbar=True, showstatusbar=True, editable=False)
+            table.show()
+            self.notebook.add(tab_frame, text=f"Band {band[0]}-{band[1]}")
 
 def main() -> None:
     """
